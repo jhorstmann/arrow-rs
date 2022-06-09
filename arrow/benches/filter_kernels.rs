2899355281404876b3ed884c7814fe0b5540522f
Jörn Horstmann <git@jhorstmann.net>
2022-06-09 10:02:06 +0200

    Experiment with avx512 filter kernels

--- arrow/benches/filter_kernels.rs
+++ arrow/benches/filter_kernels.rs
@@ -15,6 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+use std::iter;
 use std::sync::Arc;
 
 use arrow::compute::{FilterBuilder, FilterPredicate, filter_record_batch};
@@ -25,6 +26,7 @@ use arrow::compute::filter;
 use arrow::datatypes::{Field, Float32Type, Int32Type, Int64Type, Schema, UInt8Type};
 
 use arrow_array::types::Decimal128Type;
+use arrow_buffer::MutableBuffer;
 use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint;
 
@@ -36,11 +38,141 @@ fn bench_built_filter(filter: &FilterPredicate, array: &dyn Array) {
     hint::black_box(filter.filter(array).unwrap());
 }
 
+fn filter_u8_avx512(
+    array: &PrimitiveArray<UInt8Type>,
+    filter: &BooleanArray,
+) -> PrimitiveArray<UInt8Type> {
+    use std::arch::x86_64::*;
+
+    let filter_buffer = filter.values().inner();
+
+    let chunks = filter_buffer.bit_chunks(filter.offset(), filter.len());
+    let count = chunks
+        .iter()
+        .chain(iter::once(chunks.remainder_bits()))
+        .map(|chunk| chunk.count_ones() as usize)
+        .sum::<usize>();
+
+    let data_buffer = array.values();
+    let mut output_data_buffer = MutableBuffer::with_capacity(count);
+
+    let chunks = filter_buffer.bit_chunks(filter.offset(), filter.len());
+    let mut input_offset = 0;
+    let mut output_offset = 0;
+    chunks.iter().for_each(|filter_chunk| unsafe {
+        // if filter_chunk != 0 {
+        let data_chunk = _mm512_loadu_epi8(data_buffer.as_ptr().add(input_offset) as *const i8);
+        _mm512_mask_compressstoreu_epi8(
+            output_data_buffer
+                .as_mut_ptr()
+                .cast::<i8>()
+                .add(output_offset),
+            filter_chunk,
+            data_chunk,
+        );
+        output_offset += filter_chunk.count_ones() as usize;
+        // }
+        input_offset += 64;
+    });
+    if chunks.remainder_len() > 0 {
+        let filter_chunk = chunks.remainder_bits();
+        unsafe {
+            let data_chunk = _mm512_maskz_loadu_epi8(
+                filter_chunk,
+                data_buffer.as_ptr().cast::<i8>().add(input_offset),
+            );
+            _mm512_mask_compressstoreu_epi8(
+                output_data_buffer
+                    .as_mut_ptr()
+                    .cast::<i8>()
+                    .add(output_offset),
+                filter_chunk,
+                data_chunk,
+            );
+            // output_offset += filter_chunk.count_ones() as usize;
+            // input_offset += 64;
+        }
+    }
+
+    PrimitiveArray::<UInt8Type>::new(output_data_buffer.into(), None)
+}
+
+fn filter_i32_avx512(
+    array: &PrimitiveArray<Int32Type>,
+    filter: &BooleanArray,
+) -> PrimitiveArray<UInt8Type> {
+    use std::arch::x86_64::*;
+
+    let filter_buffer = filter.values().inner();
+
+    let chunks = filter_buffer.bit_chunks(filter.offset(), filter.len());
+    let count = chunks
+        .iter()
+        .chain(iter::once(chunks.remainder_bits()))
+        .map(|chunk| chunk.count_ones() as usize)
+        .sum::<usize>();
+
+    let data_buffer = array.values();
+    let mut output_data_buffer = MutableBuffer::with_capacity(count * std::mem::size_of::<i32>());
+
+    let chunks = filter_buffer.bit_chunks(filter.offset(), filter.len());
+    let mut input_offset = 0;
+    let mut output_offset = 0;
+    chunks.iter().for_each(|filter_chunk| {
+        if filter_chunk != 0 {
+            for i in 0..4 {
+                let filter_chunk = ((filter_chunk >> (i * 16)) & 0xFFFF) as u16;
+                unsafe {
+                    let data_chunk = _mm512_loadu_epi32(data_buffer.as_ptr().add(input_offset));
+                    _mm512_mask_compressstoreu_epi32(
+                        output_data_buffer
+                            .as_mut_ptr()
+                            .cast::<i32>()
+                            .add(output_offset),
+                        filter_chunk,
+                        data_chunk,
+                    );
+                }
+                output_offset += filter_chunk.count_ones() as usize;
+                input_offset += 16;
+            }
+        } else {
+            input_offset += 64;
+        }
+    });
+    if chunks.remainder_len() > 0 {
+        let filter_chunk = chunks.remainder_bits();
+        if filter_chunk != 0 {
+            for i in 0..4 {
+                let filter_chunk = ((filter_chunk >> (i * 16)) & 0xFFFF) as u16;
+                unsafe {
+                    let data_chunk = _mm512_maskz_loadu_epi32(
+                        filter_chunk,
+                        data_buffer.as_ptr().add(input_offset) as *const i32,
+                    );
+                    _mm512_mask_compressstoreu_epi32(
+                        output_data_buffer
+                            .as_mut_ptr()
+                            .cast::<i32>()
+                            .add(output_offset),
+                        filter_chunk,
+                        data_chunk,
+                    );
+                    output_offset += filter_chunk.count_ones() as usize;
+                    input_offset += 16;
+                }
+            }
+        }
+    }
+
+    PrimitiveArray::<UInt8Type>::new(output_data_buffer.into(), None)
+}
+
 fn add_benchmark(c: &mut Criterion) {
     let size = 65536;
     let filter_array = create_boolean_array(size, 0.0, 0.5);
-    let dense_filter_array = create_boolean_array(size, 0.0, 1.0 - 1.0 / 1024.0);
-    let sparse_filter_array = create_boolean_array(size, 0.0, 1.0 / 1024.0);
+    let dense_filter_array = create_boolean_array(size, 0.0, 0.9);
+    let sparse_filter_array = create_boolean_array(size, 0.0, 0.1);
 
     let filter = FilterBuilder::new(&filter_array).optimize().build();
     let dense_filter = FilterBuilder::new(&dense_filter_array).optimize().build();
@@ -70,6 +202,16 @@ fn add_benchmark(c: &mut Criterion) {
         b.iter(|| bench_filter(&data_array, &sparse_filter_array))
     });
 
+    c.bench_function("filter u8 avx512 (kept 1/2)", |b| {
+        b.iter(|| filter_u8_avx512(&data_array, &filter_array))
+    });
+    c.bench_function("filter u8 avx512 high selectivity (kept 1023/1024)", |b| {
+        b.iter(|| filter_u8_avx512(&data_array, &dense_filter_array))
+    });
+    c.bench_function("filter u8 avx512 low selectivity (kept 1/1024)", |b| {
+        b.iter(|| filter_u8_avx512(&data_array, &sparse_filter_array))
+    });
+
     c.bench_function("filter context u8 (kept 1/2)", |b| {
         b.iter(|| bench_built_filter(&filter, &data_array))
     });
@@ -89,6 +231,16 @@ fn add_benchmark(c: &mut Criterion) {
     });
     c.bench_function("filter i32 low selectivity (kept 1/1024)", |b| {
         b.iter(|| bench_filter(&data_array, &sparse_filter_array))
+    });
+
+    c.bench_function("filter i32 avx512 (kept 1/2)", |b| {
+        b.iter(|| filter_i32_avx512(&data_array, &filter_array))
+    });
+    c.bench_function("filter i32 avx512 high selectivity (kept 1023/1024)", |b| {
+        b.iter(|| filter_i32_avx512(&data_array, &dense_filter_array))
+    });
+    c.bench_function("filter i32 avx512 low selectivity (kept 1/1024)", |b| {
+        b.iter(|| filter_i32_avx512(&data_array, &sparse_filter_array))
     });
 
     c.bench_function("filter context i32 (kept 1/2)", |b| {
